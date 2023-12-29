181e67bf6021b05a5102cbaff377fefac028fed0
RAbhee <ar608146@gmail.com>
2023-12-29 10:56:45 +0530

    service setting screen added data adding

--- lib/Screens/homescreen/list_screen.dart
+++ lib/Screens/homescreen/list_screen.dart
@@ -158,9 +158,7 @@ class _ListScreenState extends State<ListScreen> {
                     ],
                   ),);
               }
-
-
-            void _toggleService(Services service) {
+              void _toggleService(Services service) {
     setState(() {
     if (selectedServices.contains(service)) {
     selectedServices.remove(service);
